aafef65756b7b8bb1d4fd9fbfaf5b1876371afb6
davistheweb <smartkelvin775@gmail.com>
2024-03-05 19:41:53 +0100

    new changes

---
+++ CSCproject.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import ttk  
+
+window = tk.Tk()
+window.title("Python GUI")
+window.geometry("700x500")
+
+lbl = tk.Label(window, text="name", font=("Sans-Serif Bold", 10))
+lbl.grid(column=0, row=0)
+
+
+def clicked():
+    lbl.configure(text="button was clicked!!")
+
+
+btn = tk.Button(window, text="Submit", command=clicked)
+btn.grid(column=2, row=0)
+
+txt = tk.Entry(window, width=10)  
+txt.grid(column=1, row=0)
+txt.focus()
+
+txt_disabled = tk.Entry(window, width=10, state='disabled')  
+txt_disabled.grid(column=4, row=0)  
+
+combo = ttk.Combobox(window)  
+combo["values"] = (1, 2, 3, "text")  
+combo.current(1)  # Set selected item
+combo.grid(column=3, row=0)
+
+# Checkbox
+chk_state = tk.BooleanVar() 
+chk_state.set(True)  # Set check state
+chk = tk.Checkbutton(window, text="choose", variable=chk_state) 
+chk.grid(column=5, row=0)
+
+# RadioButtons
+rad1 = tk.Radiobutton(window, text='First', value=1)
+rad2 = tk.Radiobutton(window, text='Second', value=2)
+rad3 = tk.Radiobutton(window, text='Third', value=3)
+rad1.grid(column=6, row=0)
+rad2.grid(column=7, row=0)
+rad3.grid(column=8, row=0)
+window.mainloop()
---
+++ Gibbor.py
@@ -0,0 +1,9 @@
+#gp calculator
+
+"""def getGradepoint(courselettergrade):
+    if (courseLetterGrade == "A"):
+        return 4.0;
+
+   """
+
+ 
